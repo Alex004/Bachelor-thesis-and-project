b38b443adff835692b6b03f1403c925efe100a12
alexandru_punga <alexandru.punga@codexworks.com>
2022-05-28 11:34:23 +0300

    delete file from main folder

--- app/src/main/java/com/example/myapplication/QRCode/googlevision/IExchangeScannedData.java
+++
@@ -1,6 +0,0 @@
-package com.example.myapplication.QRCode.googlevision;
-
-public interface IExchangeScannedData {
-
-    public void sendScannedCode(String code);
-}
--- app/src/main/java/com/example/myapplication/Utils/ConstantValue.java
+++
@@ -1,6 +0,0 @@
-package com.example.myapplication.Utils;
-
-public class ConstantValue {
-    public final static int REQUEST_QR_CODE_SCANNING_FOR_GETTING_LOCATION_INFO = 1;
-    public final static int REQUEST_QR_CODE_SCANNING_FOR_ROUTING = 2;
-}
